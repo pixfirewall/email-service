092a848b564f52a782e89d15feadf0b84ce1e818
Amir Hodaee <pixozeus@gmail.com>
2022-06-12 03:20:29 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HeathModule } from './heath/heath.module';
 
 @Module({
-  imports: [HeathModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HeathModule,
+  ],
 })
 export class AppModule {}
